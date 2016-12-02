c21bd774c84176c5f93124d6c4b0bf77edfb1f35
Jannik Steinbach <jannikac2000@gmail.com>
2016-12-02 13:41:37 +0100

    added leagueoflegends.sh

---
+++ leagueoflegends.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+env WINEPREFIX="/home/jannik/.wine32" /usr/bin/wine C:\\windows\\command\\start.exe /Unix /home/jannik/.wine32/dosdevices/c:/users/Public/Desktop/Play\ League\ of\ Legends.lnk
