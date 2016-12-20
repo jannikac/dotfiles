675f109c933cf38d6accd8c0f8461e499b44ad5b
Jannik Steinbach <jannik@debian>
2016-12-20 12:22:32 -0500

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@
 
 ###################################################################################
 
-USER=$(whoami)
+USER=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 
 function help {
@@ -17,62 +17,68 @@ function help {
   	-h, --help		prints this
   	-i, --install		installs files
 	-r, --remove		uninstalls files
-
 "
 }
 
 function install {
 	
-	# escalating to root priviliges
-	sudo -i
+	printf "
+started install..\n"
+	
+	# making directories
+	if ! [ -d "$USER/.config/" ]; then
+		mkdir -v $USER/.config
+	fi
 
-	# checking if user is root
-	if ! [ $(id -u) = 0 ]; then
-		printf "You cannot perform this operation unless you are root.\n"
-		exit
+	if ! [ -d "/etc/X11/xorg.conf.d/" ]; then
+		mkdir -v /etc/X11/xorg.conf.d/
 	fi
 
 	# replacing ~/.config/i3 with i3/
-	rm /home/$USER/.config/i3/ -rfv
-	cp -rv i3/ /home/$USER/.config/i3
+	cp -rv --remove-destination i3/ $USER/.config/i3
 	
 	# replacing ~/.bashrc with .bashrc and backing up old .bashrc
-	cp -v /home/$USER/.bashrc /home/$USER/.bashrc.old
-	cp -v .bashrc /home/$USER/.bashrc
+	cp -v --remove-destination $USER/.bashrc $USER/.bashrc.old
+	cp -v --remove-destination .bashrc $USER/.bashrc
 	
 	# replacing ~/.config/dmenu-extended/config/dmenuExtended_preferences.txt with dmenuExtended_preferences.txt
-	cp -v dmenuExtended_preferences.txt /home/$USER/.config/dmenu-extended/config/dmenuExtended_preferences.txt
+	if ! cp -v --remove-destination dmenuExtended_preferences.txt $USER/.config/dmenu-extended/config/dmenuExtended_preferences.txt; then
+	printf "\e[31mError:\e[39m dmenuExtended probrably not installed.\n"
+	fi
 	
 	# copying systemd/* to /etc/systemd/system/ and enabling pac-repos.timer and reflector.timer
-	cp -v systemd/* /etc/systemd/system/
+	cp -v --remove-destination systemd/* /etc/systemd/system/
 	systemctl enable pac-repos.timer
 	systemctl enable reflector.timer
 
 	# copying scripts/lol.sh to /usr/local/bin/lol
-	cp -v scripts/lol.sh /usr/local/bin/lol
+	cp -v --remove-destination scripts/lol.sh /usr/local/bin/lol
 
 	# copying scripts/updates.sh to /usr/lib/i3blocks/updates
-	cp -v scripts/updates.sh /usr/lib/i3blocks/updates
+	if ! cp -v --remove-destination scripts/updates.sh /usr/lib/i3blocks/updates; then
+	printf "\e[31mError:\e[39m i3blocks probrably not installed.\n"
+	fi
 
 	# copying X11/* to /etc/X11/xorg.conf.d/
-	cp -v X11/* /etc/X11/xorg.conf.d/
+	cp  -v --remove-destination X11/* /etc/X11/xorg.conf.d/
 
-	# exiting root
-	exit
-	
-
+	printf "finished install!\n
+"
 }
 
 function remove {
 	
+	printf "
+started remove..\n"
+	
 	# removing ~/.config/i3
-	rm -v /home/$USER/.config/i3/ -rf
+	rm -v $USER/.config/i3/ -rf
 	
 	# restoring original .bashrc
-	mv -v /home/$USER/.bashrc.old /home/$USER/.bashrc
+	mv -v $USER/.bashrc.old $USER/.bashrc
 	
 	# removing ~/.config/dmenu-extended/config/dmenuExtended_preferences.txt
-	rm -v /home/$USER/.config/dmenu-extended/config/dmenuExtended_preferences.txt
+	rm -v $USER/.config/dmenu-extended/config/dmenuExtended_preferences.txt
 
 	# removing /etc/systemd/system/pac-repos.timer and /etc/systemd/system/reflector.timer and disabling them
 	
@@ -91,12 +97,17 @@ function remove {
 	rm -v /usr/lib/i3blocks/updates
 
 	# removing /etc/X11/xorg.conf.d/00-keyboard.conf and /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-	sudo rm -v /etc/X11/xorg.conf.d/00-keyboard.conf
-	sudo rm -v /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-	
+	rm -v /etc/X11/xorg.conf.d/00-keyboard.conf
+	rm -v /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+
+	printf "finished remove!\n
+"	
 }
 
-
+if ! [ $(id -u) = 0 ]; then
+	printf "You cannot perform this operation unless you are root.\n"
+	exit
+fi
 
 case $1 in
 	--help|-h )
