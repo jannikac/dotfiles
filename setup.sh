894efe6469c4ad5e73cc028ac0a280cfe70f3c96
Jannik Steinbach <jannikac@mailbox.org>
2017-08-15 21:57:31 +0200

    updated setup.sh and todo.txt

--- setup.sh
+++ setup.sh
@@ -7,7 +7,6 @@
 
 ###################################################################################
 
-USER=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 # source files
 source[0]=".config/i3/config"
@@ -45,9 +44,14 @@ function help {
 
 function install {
 	
+	# checks if user is running as root		
+if ! [ $(id -u) = 0 ]; then
+	printf "You cannot perform this operation unless you are root.\n"
+	exit
+fi
+
 	printf "\nstarted install..\n\n"
-	
-	
+
 	# copies new files to destination
 	i=0
 	while [ $i -lt ${#source[*]} ]; do
@@ -62,30 +66,15 @@ function install {
 
 function remove {
 	
-	printf "
-started remove..\n"
+	printf "\nstarted remove..\n\n"
 	
-	# removing ~/.config/i3
-	rm -v $USER/.config/i3/ -rf
-	
-	# restoring original .bashrc
-	mv -v $USER/.bashrc.old $USER/.bashrc
-	
-	# removing /usr/lib/i3blocks/updates
-	rm -v /usr/lib/i3blocks/updates
 
-	# removing /etc/X11/xorg.conf.d/00-keyboard.conf and /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
-	rm -v /etc/X11/xorg.conf.d/00-keyboard.conf
-	rm -v /etc/X11/xorg.conf.d/50-mouse-acceleration.conf
+	### waiting for rewrite
 
-	printf "finished remove!\n
-"	
+	printf "\nfinished remove!\n"	
 }
 
-#if ! [ $(id -u) = 0 ]; then
-#	printf "You cannot perform this operation unless you are root.\n"
-#	exit
-#fi
+
 
 case $1 in
 	--help|-h )
