da0ed372adf2b4392f9bdec12c5c07dbf066c2f0
Jannik Steinbach <jannikac@mailbox.org>
2017-08-26 23:54:22 +0200

    revamped setup.sh

--- setup.sh
+++ setup.sh
@@ -1,41 +1,35 @@
 #!/usr/bin/env bash
 
-###################################################################################
-
-#DISCLAIMER: THIS SCRIPT IS FOR MY PERSONAL USE ONLY I WILL NOT TAKE RESPONSIBILITY
-#FOR BROKEN SYSTEMS/ LOST CONFIGURATIONS
-
-###################################################################################
-
-# required dependencies
-reqdep=(rsync printf)
+# Variables
+dir=~/dotfiles
+olddir=~/dotfiles-old
 
 # optional dependencies
 opdep=(rofi i3 i3blocks xrandr xinit feh teamspeak3 xautolock physlock urxvt zsh upgrade_oh_my_zsh)
 
 # source files
-source[0]=".config/i3/config"
-source[1]=".config/i3/i3blocks.conf"
-source[2]=".config/rofi/config"
-source[3]=".zshrc"
-source[4]=".Xresources"
+source[0]="i3/config"
+source[1]="i3/i3blocks.conf"
+source[2]="rofi"
+source[3]="Xresources"
+source[4]="zshrc"
 
-source[5]="usr/lib/i3blocks/updates"
-source[6]="etc/X11/xinit/xinitrc.d/monitorsetup.sh"
-source[7]="etc/X11/xorg.conf.d/00-keyboard.conf"
-source[8]="etc/X11/xorg.conf.d/50-mouse-acceleration.conf"
+#source[5]="usr/lib/i3blocks/updates"
+#source[6]="etc/X11/xinit/xinitrc.d/monitorsetup.sh"
+#source[7]="etc/X11/xorg.conf.d/00-keyboard.conf"
+#source[8]="etc/X11/xorg.conf.d/50-mouse-acceleration.conf"
 
 # destination files
-dest[0]="$HOME/${source[0]}"
-dest[1]="$HOME/${source[1]}"
-dest[2]="$HOME/${source[2]}"
-dest[3]="$HOME/${source[3]}"
-dest[4]="$HOME/${source[4]}"
+dest[0]="~/.config/$source[1]"
+dest[1]="~/.config/$source[2]"
+dest[2]="~/.config/rofi/config"
+dest[3]="~/.Xresources"
+dest[4]="~/.zshrc"
 
-dest[5]="/${source[5]}"
-dest[6]="/${source[6]}"
-dest[7]="/${source[7]}"
-dest[8]="/${source[8]}"
+#dest[5]="/${source[5]}"
+#dest[6]="/${source[6]}"
+#dest[7]="/${source[7]}"
+#dest[8]="/${source[8]}"
 
 function help {
 	printf "
@@ -44,98 +38,39 @@ function help {
   	-h, --help		prints this
 	-i, --install		installs files (OVERWRITES EXISTING FILES)
 	-r, --remove		uninstalls files
-	-d, --dependencies	checks for optional and required dependencies
 "
 }
-
-function isPackageInstalled() {
-	which $i &> /dev/null && echo $?
-
-}
-
-function dependencies {
-	# checking required dependencies
-
-
-	printf "Checking required dependencies..\n"
-
-	for i in ${reqdep[@]}
-	do
-		if [ $(isPackageInstalled '$1') ]; then
-			printf "$i..ok\n"
-		else
-			printf "$i..not found\n"
-			printf "ERROR: required package \"$i\" was not found, exiting\n"
-			exit
-		fi
-	done
-	
-	printf "all reqired dependencies installed!\n"
-
-	# checking optional dependencies
-	
-	printf "checking optional dependencies..\n"
-	
-	missingPackage=false
-	
-	for i in ${opdep[@]}
-	do
-		if [ $(isPackageInstalled '$1') ]; then
-			printf "$i.. ok\n"
-		else
-			printf "$i.. not found\n"
-			missingPackage=true
-		fi
-	done
-	
-	if [ "$missingPackage" = false ]; then
-			printf "all optional dependencies installed!\n"
-		fi
-	}
 
 	
 
 function install {
 	
 	# checks if user is running as root		
-	if ! [ $(id -u) = 0 ]; then
-		printf "You cannot perform this operation unless you are root.\n"
-		exit
-	fi
+	#if ! [ $(id -u) = 0 ]; then
+	#	printf "You cannot perform this operation unless you are root.\n"
+	#	exit
+	#fi
 	
-	dependencies	
+	# Move any existing dotfiles to $olddir
+	prinff "Moving any existing dotfiles to $olddir\n"
+	mkdir $olddir
+	for i in ${dest[@]}
+	do
+		mv $i ~/dotifles-old
+	done
 
-	# informing user about not installed package
+	
+	printf "Started install..\n"
 
-	if [ "$missingPackage" = true ]; then
-		printf "one or more missing optional dependencies found.\n"
-		printf "continue anyway?(N/y) "
-		read input
-		case $input in
-			Y|y|yes )
-				;;
-			N|n|no )
-				exit
-				;;
-			* )
-				exit
-				;;
-		esac
-	fi
-
-
-	printf "\nstarted install..\n\n"
-
-	# copies new files to destination
+	# symlinking files
 	i=0
 	while [ $i -lt ${#source[*]} ]; do
-		rsync -av --delete ${source[$i]} ${dest[$i]}
+		ln -sv ${source[$i]} ${dest[$i]}
 		i=$(( $i + 1 ));
 	done
-		
 
 
-	printf "\nfinished install!\n"
+	printf "Finished install\n"
 }
 
 function remove {
